55712a8c88ed5a24e07be699d992c632539676e1
Prasenjeet0808 <prasenjeet63673@gmail.com>
2026-02-10 00:17:41 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://one-examnotesaiclient-5wa7.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
@@ -49,4 +49,5 @@ app.use("/api/credit",creditRouter)
 app.listen(PORT,()=>{
     console.log(`✅ Server running on port ${PORT}`)
     connectDb()
+
 })
